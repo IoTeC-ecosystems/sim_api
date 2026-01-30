c938d4887534f8011b223af1ab2f3dcd52c4c7d6
Jose Septien <jaseptienh@gmail.com>
2026-01-30 17:48:51 -0600

    Add basic module structure.

---
+++ include/sim_api.h
@@ -0,0 +1,12 @@
+#ifndef SIM_API_H
+#define SIM_API_H
+
+#include <stdio.h>
+
+#include "at.h"
+#include "periph/uart.h"
+#include "ztimer.h"
+
+void sim_api_init(void);
+
+#endif // SIM_API_H
---
+++ sim_api.c
@@ -0,0 +1,6 @@
+#include "sim_api.h"
+
+void sim_api_init(void)
+{
+    printf("SIM API initialized.\n");
+}
